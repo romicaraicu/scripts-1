93a9701b7b16cb4fc05fe7ecd423c1b0a08de9d0
Roman Kuznetsov <roman@kuznero.com>
2017-02-01 14:05:51 +0100

    Introduce shell/monitor.sh

---
+++ shell/monitor.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [[ "$#" -ne 3 ]]; then
+  echo "Expected arguments are missing: EXTENSIONS DELAY COMMAND"
+  exit 1
+fi
+
+IFS=',' read -r -a exts <<< "$1"
+delay=$2
+cmd=$3
+
+while true; do
+  inotifywait -q -r -e moved_to,moved_from ./ | while read path events file; do
+
+    for ext in "${exts[@]}"; do
+      if [[ $file =~ .$ext$ ]]; then
+        echo "$file has changed in $path, triggering build ..."
+        sleep $delay
+        eval $cmd
+      fi
+    done
+
+  done
+done
