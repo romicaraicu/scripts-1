c2025696b23ea55feaf1448049f185de9b6e7007
Roman Kuznetsov <roman@kuznero.com>
2017-02-06 16:06:23 +0100

    Trigger command before setting up monitoring

--- shell/monitor.sh
+++ shell/monitor.sh
@@ -9,12 +9,14 @@ IFS=',' read -r -a exts <<< "$1"
 delay=$2
 cmd=$3
 
+eval $cmd
+
 while true; do
   inotifywait -q -r -e moved_to,moved_from ./ | while read path events file; do
 
     for ext in "${exts[@]}"; do
       if [[ $file =~ .$ext$ ]]; then
-        echo "$file has changed in $path, triggering build ..."
+        echo "$file has changed in $path, triggering command ..."
         sleep $delay
         eval $cmd
       fi
