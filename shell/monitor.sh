4ca3f90a28b8b4bbc15d81235ed6a95eb72b7f18
Roman Kuznetsov <roman@kuznero.com>
2017-02-14 08:38:01 +0100

    Properly handle delay when monitoring file changes

--- shell/monitor.sh
+++ shell/monitor.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+command -v inotifywait >/dev/null 2>&1 || { echo >&2 "[inotifywait] is required, but not installed.  Aborting."; exit 1; }
+command -v date >/dev/null 2>&1 || { echo >&2 "[date] is required, but not installed.  Aborting."; exit 1; }
+
 if [[ "$#" -ne 3 ]]; then
   echo "Expected arguments are missing: EXTENSIONS DELAY COMMAND"
   exit 1
@@ -9,23 +12,20 @@ IFS=',' read -r -a exts <<< "$1"
 delay=$2
 cmd=$3
 
-RED='\033[1;31m'
-GREEN='\033[1;32m'
-BLUE='\033[1;34m'
-NC='\033[0m'
-
 function run () {
   eval $cmd
 }
 
 run
+dt=$(date +%s)
 
 inotifywait -q -r -m -e moved_to,moved_from ./ | while read path events file; do
   for ext in "${exts[@]}"; do
     if [[ $file =~ .$ext$ ]]; then
-      echo -e "${GREEN}$file${NC} has changed in ${BLUE}$path${NC}, triggering command ..."
-      sleep $delay
-      run
+      if [[ $(date +%s) > $(($dt + $delay)) ]]; then
+        run
+        dt=$(date +%s)
+      fi
     fi
   done
 done
