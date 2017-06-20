f24ab1e11a7d6a118e1f6d49805c1b82f435561e
Roman Kuznetsov <roman@kuznero.com>
2017-06-20 14:16:38 +0200

    Introduce warning level for checks

--- monitoring/monitor.sh
+++ monitoring/monitor.sh
@@ -10,22 +10,16 @@ LOCK="$ROOT/lock"
 CHECKS_OUTPUT="$ROOT/checks-output"
 PASSED_OUTPUT="$ROOT/passed-output"
 FAILED_OUTPUT="$ROOT/failed-output"
+WARNED_OUTPUT="$ROOT/warned-output"
 NOTIFY_TIME=$((5 * 1000))
 IMAGE_SIZE=24
-
-function xnotify_healthy () {
-  notify-send -t $NOTIFY_TIME -i "$ROOT/icons/healthy.png" "$1" "$2"
-}
-
-function xnotify_sick () {
-  notify-send -t $NOTIFY_TIME -i "$ROOT/icons/sick.png" "$1" "$2"
-}
 
 function run_checks () {
   echo "" > $CHECKS_OUTPUT
   checks=($(find "$ROOT/checks" -type f -iregex ".*\.sh$"))
   offset=$((${#ROOT} + 8))
   failed_checks=0
+  warned_checks=0
   passed_checks=0
   for check in "${checks[@]}"; do
     echo "# ${check:$offset} ..." >> $CHECKS_OUTPUT
@@ -34,28 +28,44 @@ function run_checks () {
     if [ "$rc" -eq "0" ]; then
       passed_checks=$(($passed_checks + 1))
       echo " >> PASSED" >> $CHECKS_OUTPUT
-    else
+    elif [ "$rc" -eq "1" ]; then
       failed_checks=$(($failed_checks + 1))
       echo " >> FAILED" >> $CHECKS_OUTPUT
+    else
+      warned_checks=$(($warned_checks + 1))
+      echo " >> WARNED" >> $CHECKS_OUTPUT
     fi
     echo "" >> $CHECKS_OUTPUT
   done
   echo "$failed_checks" > $FAILED_OUTPUT
+  echo "$warned_checks" > $WARNED_OUTPUT
   echo "$passed_checks" > $PASSED_OUTPUT
 }
 
 output=$(cat $CHECKS_OUTPUT 2> /dev/null)
 passed=$(cat $PASSED_OUTPUT 2> /dev/null)
 failed=$(cat $FAILED_OUTPUT 2> /dev/null)
-icon=$(if [ "$failed" -gt "0" ]; then echo "sick"; else echo "healthy"; fi)
+warned=12 # $(cat $WARNED_OUTPUT 2> /dev/null)
+icon=$(if [ "$failed" -gt "0" ]; then echo "sick"; elif [ "$warned" -gt "0" ]; then echo "dizzy"; else echo "healthy"; fi)
+
+function xnotify () {
+  notify-send -t $NOTIFY_TIME -i "$ROOT/icons/$icon.png" "$1" "$2"
+}
+
+function show_log () {
+  printf "Failed: $failed\nWarned: $warned\nPassed: $passed\n\n$output" | vim -g -
+}
 
 ( flock -x 200
 if [ "$FLAG" == "-n" ]; then
-  printf "Failed: $failed\nPassed: $passed\n\n$output" | vim -g -
   if [ "$failed" -gt "0" ]; then
-    xnotify_sick "Sick" "System has some failing checks"
+    xnotify "Sick" "System has some failing checks"
+    $(show_log)
+  elif [ "$warned" -gt "0" ]; then
+    xnotify "Dizzy" "System has some warning checks"
+    $(show_log)
   else
-    xnotify_healthy "Healthy" "System seams to be fully operational"
+    xnotify "Healthy" "System seams to be fully operational"
   fi
 else
   $(run_checks)
