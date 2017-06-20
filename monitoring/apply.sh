3e48d8b38f599fa46caa8aced30700437e980d64
Roman Kuznetsov <roman@kuznero.com>
2017-06-21 01:06:31 +0200

    Add more pretty-printers

--- monitoring/apply.sh
+++ monitoring/apply.sh
@@ -2,6 +2,7 @@
 
 command -v find >/dev/null 2>&1 || { echo >&2 "[find] is required, but not installed.  Aborting."; exit 1; }
 command -v sed >/dev/null 2>&1 || { echo >&2 "[sed] is required, but not installed.  Aborting."; exit 1; }
+command -v printf >/dev/null 2>&1 || { echo >&2 "[printf] is required, but not installed.  Aborting."; exit 1; }
 
 if [[ "$#" -ne 2 ]]; then
   echo "Expected arguments are missing: TARGET_HOST RABBITMQ_CREDENTIALS"
@@ -10,18 +11,25 @@ fi
 
 RED='\033[1;31m'
 GREEN='\033[1;32m'
+CYAN='\033[0;36m'
+GRAY='\033[1;30m'
 NC='\033[0m'
 
 TARGET_HOST=$1
 RABBITMQ_CREDENTIALS=$2
 ROOT="$(dirname "$(readlink -f "$0")")"
 
+function format () {
+  line='--------------------------------------------------------------------------------'
+  PROC_NAME=$1
+  printf "${RED}#${NC} ${CYAN}%s${NC} ${GRAY}%s${NC} [${GREEN}DONE${NC}]\n" $PROC_NAME "${line:${#PROC_NAME}}"
+}
+
 inputs=($(find "$ROOT/checks" -type f -iregex ".*\.input$"))
 offset=$((${#ROOT} + 8))
 echo ""
 for input in "${inputs[@]}"; do
-  echo -n -e "${RED}#${NC} ${input:$offset} ... "
   sed -i "s/127.0.0.1/$TARGET_HOST/" $input
   sed -i "s/guest:guest/$RABBITMQ_CREDENTIALS/" $input
-  echo -e "${GREEN}done${NC}"
+  format "${input:$offset}"
 done
--- monitoring/test.sh
+++ monitoring/test.sh
@@ -9,32 +9,60 @@ RED='\033[1;31m'
 YELLOW='\033[1;33m'
 GREEN='\033[1;32m'
 CYAN='\033[0;36m'
+GRAY='\033[1;30m'
 NC='\033[0m'
+
+function format_passed () {
+  line='--------------------------------------------------------------------------------'
+  PROC_NAME=$1
+  printf "${RED}#${NC} ${CYAN}%s${NC} ${GRAY}%s${NC} [${GREEN}PASSED${NC}]\n" $PROC_NAME "${line:${#PROC_NAME}}"
+}
+
+function format_warned () {
+  line='--------------------------------------------------------------------------------'
+  PROC_NAME=$1
+  printf "${RED}#${NC} ${CYAN}%s${NC} ${GRAY}%s${NC} [${YELLOW}WARNED${NC}]\n" $PROC_NAME "${line:${#PROC_NAME}}"
+}
+
+function format_failed () {
+  line='--------------------------------------------------------------------------------'
+  PROC_NAME=$1
+  printf "${RED}#${NC} ${CYAN}%s${NC} ${GRAY}%s${NC} [${RED}FAILED${NC}]\n" $PROC_NAME "${line:${#PROC_NAME}}"
+}
 
 failed_checks=0
 warned_checks=0
 passed_checks=0
 
+function format_output () {
+  IFS='|' read -r -a output <<< "$1"
+  for line in "${output[@]}"; do
+    echo -e "  ${YELLOW}|${NC} ${GRAY}$line${NC}"
+  done
+}
+
 echo ""
 checks=($(find "$ROOT/checks" -type f -iregex ".*\.sh$"))
 offset=$((${#ROOT} + 8))
 for check in "${checks[@]}"; do
-  echo -e "# ${CYAN}${check:$offset}${NC} ..."
-  $check
+  output=$($check)
   rc=$?
+  output=$(echo "${output[0]}" | sed ':a;N;$!ba;s/\n/|/g')
   if [ "$rc" -eq "0" ]; then
     passed_checks=$(($passed_checks + 1))
-    echo -e " >> ${GREEN}PASSED${NC}"
+    format_passed "${check:$offset}"
   elif [ "$rc" -eq "1" ]; then
     failed_checks=$(($failed_checks + 1))
-    echo -e " >> ${RED}FAILED${NC}"
+    format_failed "${check:$offset}"
+    format_output "$output"
   else
     warned_checks=$(($warned_checks + 1))
-    echo -e " >> ${YELLOW}WARNED${NC}"
+    format_warned "${check:$offset}"
+    format_output "$output"
   fi
-  echo ""
 done
 
+echo ""
 echo -e "${GRAY}Failed:${NC} ${RED}$failed_checks${NC}"
 echo -e "${GRAY}Warned:${NC} ${YELLOW}$warned_checks${NC}"
 echo -e "${GRAY}Passed:${NC} ${GREEN}$passed_checks${NC}"
