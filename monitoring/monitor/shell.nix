54e5233849eded486a8df5eb8e569ae2ce2b2fd4
Roman Kuznetsov <roman@kuznero.com>
2017-07-05 12:24:05 +0200

    Introduce blank monitor aggregator project

---
+++ monitoring/monitor/shell.nix
@@ -0,0 +1,37 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, stdenv
+      , ghc-mod, hlint, hoogle, hindent, stylish-haskell
+      }:
+      mkDerivation {
+        pname = "monitor";
+        version = "0.0.1.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [ base ];
+        buildDepends = [ ghc-mod hlint hoogle hindent stylish-haskell ];
+        homepage = "https://github.com/kuznero/scripts/monitoring/monitor#README";
+        description = "Monitoring aggregator";
+        license = stdenv.lib.licenses.mit;
+      };
+
+  haskellPackages = (if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler})
+                    .override {
+                      overrides = self: super:
+                      {
+                        ghc-syb-utils = pkgs.haskell.lib.dontCheck super.ghc-syb-utils;
+                      };
+                    };
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
