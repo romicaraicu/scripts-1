2acf17c3b6eaea0541e7214a51ea8d9dfd78f11f
Roman Kuznetsov <roman@kuznero.com>
2017-07-06 16:28:45 +0200

    Optimize

--- monitoring/monitor/docker.nix
+++ monitoring/monitor/docker.nix
@@ -3,11 +3,26 @@
 with pkgs;
 dockerTools.buildImage {
   name = "monitor";
-  contents = [ (import ./shell.nix {}) ];
+  contents = [
+    (import ./default.nix {})
+    bash
+    coreutils
+    jq
+    curl
+  ];
+  runAsRoot = ''
+    #!${stdenv.shell}
+    ${dockerTools.shadowSetup}
+    mkdir /checks
+  '';
   config = {
     Cmd = [ "monitor" ];
     ExposedPorts = {
       "3000/tcp" = {};
+    };
+    WorkingDir = "/checks";
+    Volumes = {
+      "/checks" = {};
     };
   };
 }
