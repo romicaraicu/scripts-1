3ab767d3a6ee40748cf438885fb2a8ead3860909
Roman Kuznetsov <roman@kuznero.com>
2017-07-11 09:57:45 +0200

    Patch shebang

--- monitoring/monitor/checks.nix
+++ monitoring/monitor/checks.nix
@@ -3,7 +3,10 @@
 stdenv.mkDerivation {
   name = "checks";
   src = ../checks;
-  phases = [ "unpackPhase" "installPhase" ];
+  phases = [ "patchPhase" "unpackPhase" "installPhase" ];
+  patchPhase = ''
+    patchShebangs .
+  '';
   installPhase = ''
     mkdir -p $out/checks
     cp -r ../checks/* $out/checks/
